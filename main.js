c4e5f9dd49ffd4ee6692f1e508adc4e02d5344c3
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2020-08-11 21:06:15 +0700

    Updated V2.0

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 /*
 * Coded by: GuckTube YT
+* Helped by: Clayne
 * Credit Discord Bot: eslachance
 */
 const Discord = require("discord.js");
-var exec = require('child_process').execFile;
+const { exec } = require("child_process");
+const fs = require('fs')
 const client = new Discord.Client();
 const config = require("./config.json");
-
+const group = ("Server Creator")
 client.on("ready", () => {
   console.log(`Bot is Online Now!`); 
   client.user.setActivity(`GTPSController By GuckTube YT`);
@@ -23,20 +25,32 @@ client.on("message", async message => {
   }
 
   if(command === "start") {
-    if(!message.member.roles.cache.some(r=>["Onwer Role"].includes(r.name)) )
+    if(!message.member.roles.cache.some(r=>[group].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
+
+      /* or you can use this code
+      if(message.author.id !== "User ID") return message.channel.send('Sorry, you don't have permissions to use this!')
+      Helped by Clayne
+      */
+
       const m = await message.channel.send("Please Wait...");
-   exec('enet.exe', function(err, data) {  
-    return m.edit("enet.exe not found! please change your gtps exe to enet.exe, and put this app to your folder gtps");
-    });  
-      m.edit("Server Has Bees Started!");
+      try {
+        if (fs.existsSync("enet.exe")) {
+          exec("start enet.exe")
+          m.edit("Server is UP")
+        }
+      } catch(err) {
+        m.edit("enet.exe Not Found! Please put this app into your gtps folder")
+      }
+      
+      
   }
 
   if(command === "stop") {
-    if(!message.member.roles.cache.some(r=>["Owner Role"].includes(r.name)) )
+    if(!message.member.roles.cache.some(r=>[group].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
       exec("taskkill /f /im enet.exe")
-      message.channel.send("Server Has Bees Stopped!");
+      message.channel.send("Server Has Been Stopped!");
   }
 });
 
