08dfc45fd5f30c5def6241a65844491eb865b2f8
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2020-08-13 17:24:30 +0700

    Updated V4.0

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
 * Coded by: GuckTube YT
 * Helped by: Clayne
-* Credit Discord Bot: eslachance
+* Credit Discord Bot example Code: eslachance
 */
 const Discord = require("discord.js");
 const { exec } = require("child_process");
@@ -9,7 +9,7 @@ const kill = require("child_process").exec
 const fs = require('fs')
 const client = new Discord.Client();
 const config = require("./config.json");
-const role = ("Server Creator")
+
 client.on("ready", () => {
   console.log(`Bot is Online Now!`); 
   client.user.setActivity(`GTPSController By GuckTube YT`);
@@ -22,11 +22,11 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
 
   if(command === "help") {
-    message.channel.send("```gt!start (Start the server) (Owner Only)\ngt!stop (Stop the server) (Owner Only)```");
+    message.channel.send("```gt!start (Start the server) (Owner Only)\ngt!stop (Stop the server) (Owner Only)\ngt!count (Count The Players and Worlds)\ngt!maintenance [on/off] (Maintenance Switch)```");
   }
 
   if(command === "start") {
-    if(!message.member.roles.cache.some(r=>[role].includes(r.name)) )
+    if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
       const m = await message.channel.send("Please Wait...");
       try {
@@ -40,20 +40,105 @@ client.on("message", async message => {
   }
 
   if(command === "stop") {
-    if(!message.member.roles.cache.some(r=>[role].includes(r.name)) )
+    if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
-      kill("taskkill.exe /f /im enet.exe")
+      kill("taskkill /f /im enet.exe")
       message.channel.send("Server Has Been Stopped!");
   }
 
   if(command === "count") {
-    if(!message.member.roles.cache.some(r=>[role].includes(r.name)) )
+    if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
       const m = await message.channel.send("Please Wait...");
-      const f1 = fs.readdirSync('player').length
-      const f2 = fs.readdirSync('worlds').length
-m.edit("Player Count = " + f1 + "\nWorlds Count = " + f2);
+      fs.readdir(config.player, (err, files) => {
+      if (err)
+      {
+        m.edit("Player Folder Not Found! Please edit on config.json")
+      }
+      fs.readdir(config.world, (err1, files1) => {
+        if (err1)
+      {
+        m.edit("World Not Found! Please edit on config.json")
+      }
+      const f1 = files.length;
+      const f2 = files1.length;
+      m.edit("Player Count = " + f1 + "\nWorlds Count = " + f2);
+      })});;
   }
+  if (command === "maintenance")
+  {
+    if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+      if (!args.length)
+      return message.channel.send('Command = gt!maintenance [on/off]')
+
+      const m = await message.channel.send("Please Wait...");
+      const command1 = args.shift().toLowerCase();
+      if (command1 === "on")
+      {
+        if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+        fs.readFile(config.sdata, 'utf8', function (err,data) {
+          if (err) {
+            return m.edit("Error: server_data.php Not Found! please set in config.json");
+          }
+        var result = data.replace("#maint|", 'maint|');
+        fs.writeFile(config.sdata, result, 'utf8', function (err) {
+          if (err) 
+          return m.edit("Error: Server is already maintenance");
+        });
+        });
+        return m.edit("Maintenance Server is Turn On!");
+      }
+      if (command1 === "off")
+      {
+        if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+        fs.readFile(config.sdata, 'utf8', function (err,data1) {
+          if (err) {
+            return m.edit("Error: server_data.php Not Found! please set in config.json");
+          }
+        var result1 = data1.replace("maint|", '#maint|');
+        fs.writeFile(config.sdata, result1, 'utf8', function (err) {
+          if (err) 
+          return m.edit("Error: Server isnt maintenance");
+        });
+        });
+        return m.edit("Maintenance Server is Turn Off");
+        }
+      }
+      if (command === "pdelete")
+      {
+        if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+        if (!args.length)
+      return message.channel.send('Command = gt!pdelete [player]')
+      const m = await message.channel.send("Please Wait...");
+      fs.unlink(config.player + "\\" + args + ".json", function (err) {
+        if (err)
+        return m.edit("Player Not Found!");
+        m.edit('Player has been Deleted! Restarting...');
+        kill("taskkill /f /im enet.exe")
+        exec("start enet.exe")
+        m.edit("Server Has Been Restarted!")
+      });
+    }
+    if (command === "wdelete")
+      {
+        if(!message.member.roles.cache.some(r=>[config.role].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+        if (!args.length)
+      return message.channel.send('Command = gt!wdelete [player]')
+      const m = await message.channel.send("Please Wait...");
+      fs.unlink(config.world + "\\" + args + ".json", function (err) {
+        if (err)
+        return m.edit("World Not Found!");
+        m.edit('World has been Deleted! Restarting Server...');
+        kill("taskkill /f /im enet.exe")
+        exec("start enet.exe")
+        m.edit("Server Has Been Restarted!")
+      });
+    }
 });
 
 client.login(config.token);
